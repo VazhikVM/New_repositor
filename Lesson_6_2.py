159011b51693a54661a9dfff73fa0276ecd76005
vazhik <v.vazhinskiy@yandex.ru>
2021-01-02 16:35:44 +0300

    готово

---
+++ Lesson_6_2.py
@@ -0,0 +1,15 @@
+class Road:
+    mass = 25
+    height = 5
+
+    def __init__(self, length, width):
+        self._l = length
+        self._w = width
+
+    def asphalt(self):
+        sum_asphalt = (self._l * self._w * Road.mass * Road.height)/1000
+        return sum_asphalt
+
+
+street_1 = Road(float(input("Введите длину дороги в метрах: ")), float(input("Введите ширину дороги в метрах: ")))
+print(f'Для покрытия дороги необходимо {round(street_1.asphalt(), 2)} тонн асфальта')
